883a4e5900a1187f6f37d7d1bbecc5d40bca25ec
System Administrator <root@ZengYingQiongs-Mac-mini.local>
2020-12-11 18:27:27 +0800

    install

---
+++ invoice/config.py
@@ -0,0 +1,12 @@
+
+TEST = False
+
+DATABASE = {
+  'ENGINE': 'django.db.backends.mysql',
+  'NAME': 'invoice',
+  'HOST': 'crm01.svr.up5d.com',
+  'PORT': 3306,
+  'USER': 'root',
+  'PASSWORD': 'VcrTing.ZT123zlt',
+  'CONN_MAX_AGE': 9
+}
