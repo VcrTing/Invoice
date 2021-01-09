dbe9d1ac342d5a912c6eab028acb44ba5abed0a5
System Administrator <root@ZengYingQiongs-Mac-mini.local>
2021-01-09 19:56:36 +0800

    0.0.11

--- Appis/freight/serializers.py
+++ Appis/freight/serializers.py
@@ -16,12 +16,42 @@ class FreightSerializer(serializers.ModelSerializer):
         清单
     """
     tag_id = serializers.IntegerField(required = False)
+    tags = serializers.CharField(required = False)
 
     class Meta:
         model = models.Freight
         depth = 3
-        fields = ['id', 'num', 'named', 'unit', 'tag', 'tag_id', 'price', 'is_n', 'status']
+        fields = ['id', 'num', 'named', 'unit', 'tags', 'tag', 'tag_id', 'price', 'is_n', 'status']
         
+    def create(self, validated_data):
+        tags = validated_data.pop('tags')
+        
+        tags = [int(i) for i in str(tags).split(',')]
+        instance = super().create(validated_data)
+        instance.tag.clear()
+        for t in tags:
+            instance.tag.add(t)
+
+        instance.save()
+        
+        return instance
+
+    def update(self, instance, validated_data):
+        try:
+            tags = validated_data.pop('tags')
+            tags = [int(i) for i in str(tags).split(',')]
+
+            super().update(instance, validated_data)
+
+            instance.tag.clear()
+            for t in tags:
+                instance.tag.add(t)
+
+            instance.save()
+        except:
+            super().update(instance, validated_data)
+
+        return instance
 """
 class FreightEveryMemberSerializer(serializers.ModelSerializer):
     ""
