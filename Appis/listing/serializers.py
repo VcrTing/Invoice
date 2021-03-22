8d928b6645184b96ae5a24f101345ca8f39e3aee
System Administrator <root@ZengYingQiongs-Mac-mini.local>
2021-03-22 21:32:26 +0800

    1.1.3

--- Appis/listing/serializers.py
+++ Appis/listing/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from . import models
 
-from Appis.member.models import Membery, PriceCollect
+from Appis.member.models import Membery, PriceCollect, PayTime
 
 # Serializer
 class ListingSerializer(serializers.ModelSerializer):
@@ -9,13 +9,14 @@ class ListingSerializer(serializers.ModelSerializer):
         清单
     """
     membery_id = serializers.IntegerField(required = False)
+    pay_time_id = serializers.IntegerField(required = False)
     price_collect_id = serializers.IntegerField(required = False)
 
     class Meta:
         model = models.Listing
         depth = 3
         fields = [
-            'id', 'num', 'listing_time', 'pay_way',
+            'id', 'num', 'listing_time', 'pay_way', 'pay_time', 'pay_time_id', 
             'membery', 'membery_id',
             'price_collect', 'price_collect_id',
             'freight_item_num', 'result_price', 'total_price_freight',
@@ -26,10 +27,12 @@ class ListingSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         membery_id = validated_data.pop('membery_id')
+        pay_time_id = validated_data.pop('pay_time_id')
         price_collect_id = validated_data.pop('price_collect_id')
         instance = super().create(validated_data)
         
         instance.membery = Membery.objects.get(id = membery_id)
+        instance.pay_time = PayTime.objects.get(id = pay_time_id)
         instance.price_collect = PriceCollect.objects.get(id = price_collect_id)
         instance.save()
         
